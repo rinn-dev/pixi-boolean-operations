89e1269a5ba00b32ca4b61faead6d6acaaaf9ca5
rinn-dev <mr.htetmyatlinn404@gmail.com>
2023-10-18 20:12:41 +0630

    chore: add some workarounds for interactive events

--- src/pixi/scene.js
+++ src/pixi/scene.js
@@ -1,4 +1,4 @@
-import { Application } from "pixi.js-legacy";
+import { Application, Assets, Sprite } from "pixi.js-legacy";
 
 /**
  * Render the pixi application
@@ -18,5 +18,23 @@ export async function renderScenes(app, container) {
     // Source - https://github.com/pixijs/pixijs/issues/5111
     app.renderer.plugins.accessibility.destroy();
     delete app.renderer.plugins.accessibility;
+
+    // Set initial event mode for stage
+    app.stage.eventMode = "static";
+
+    /**
+     * Registering interaction events on Stage or Container won't be working without a Sprite
+     * You can remove if you have already at least one Sprite in your Pixi application
+     */
+    const transparentTexture = await Assets.load("/transparent.png");
+    const placeHolderSprite = new Sprite(transparentTexture);
+    placeHolderSprite.width = app.screen.width;
+    placeHolderSprite.height = app.screen.height;
+    app.stage.addChild(placeHolderSprite);
+
+    // Disable right click context menu
+    app.view.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
   }
 }
