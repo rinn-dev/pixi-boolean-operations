8780b4b1f3b4fbc172170f6d39251d3523880e3a
rinn-dev <mr.htetmyatlinn404@gmail.com>
2023-10-18 19:55:10 +0630

    chore: disable accessibility manager

--- src/pixi/scene.js
+++ src/pixi/scene.js
@@ -12,5 +12,11 @@ export async function renderScenes(app, container) {
   if (typeof clientWidth == "number") {
     // Setting canvas width and height with 16/9 ratio
     app.renderer.resize(clientWidth, clientWidth * 0.5625);
+
+    // Destroy accessibility plugin that might cause some issues while clicking Tab key or Return key in IOS
+    // It might cover the canvas element with a div with the absolute position and blocking out all of the interactions on Sprites, Stage and Containers.
+    // Source - https://github.com/pixijs/pixijs/issues/5111
+    app.renderer.plugins.accessibility.destroy();
+    delete app.renderer.plugins.accessibility;
   }
 }
