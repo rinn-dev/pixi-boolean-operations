5a466f67804d8a27f2214163417163ff28baf004
rinn-dev <b8_htetmyatlinn@uit.edu.mm>
2023-10-24 12:40:23 +0000

    feat: set select mode as default mode

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import "../styles/global.css";
 import { initApplication } from "./pixi";
 import { bindModeEvents } from "./pixi/modes";
-import { pixiStore } from "./services/Store";
+import { MODE, pixiStore } from "./services/Store";
 
 window.addEventListener("DOMContentLoaded", async () => {
   const container = document.querySelector(".canvas-container");
   const app = await initApplication(container);
   window.pixiStore = pixiStore;
   bindModeEvents(app);
+
+  // Set default mode
+  pixiStore[MODE] = "select";
 });
