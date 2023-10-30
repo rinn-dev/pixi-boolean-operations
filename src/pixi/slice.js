9b5f423893f4445a198d1ec563d21d0e52bf340e
rinn-dev <mr.htetmyatlinn404@gmail.com>
2023-10-30 16:47:20 +0630

    feat: add docs and error display for slicing

--- src/pixi/slice.js
+++ src/pixi/slice.js
@@ -1,20 +1,19 @@
 import polybool from "polybooljs";
+import { POLYGONS, pixiStore } from "../services/Store";
+import { completePolygon, polygonsToExecutable } from "../utils";
 
 /**
  * A function that slices the intersecting polygon into multiple polygons
  * @param {number[]} sliceBoundary - The slice boundary points
  * @returns {void}
  */
-
-import { POLYGONS, pixiStore } from "../services/Store";
-import { completePolygon, polygonsToExecutable } from "../utils";
-
 export function slice(sliceBoundary) {
   const polygons = pixiStore[POLYGONS];
   let targetPolygon;
   let targetIndex;
   const boundary = polygonsToExecutable(sliceBoundary);
 
+  // Check the first intersecting polygon
   for (let i = 0; i < polygons.length; i++) {
     const polygonPoints = polygonsToExecutable(polygons[i]);
     const result = polybool.intersect(polygonPoints, boundary);
@@ -27,13 +26,28 @@ export function slice(sliceBoundary) {
 
   if (targetPolygon != undefined) {
     const difference = polybool.difference(targetPolygon, boundary);
-    if (difference.regions.length === 2) {
+
+    // If the difference is more than 2, it means the polygon slicing is successful
+    if (difference.regions.length > 2) {
+      // Transform the difference into multiple polygons
       const newPolygons = difference.regions.map((polygon) =>
         completePolygon(polygon.flat())
       );
+
+      // Update the store with the new polygons
       pixiStore[POLYGONS] = polygons
         .filter((_, index) => index !== targetIndex)
         .concat(newPolygons);
     }
+  } else {
+    // Display error on slice fails
+    dispatchEvent(
+      new CustomEvent("pixiError", {
+        detail: {
+          message:
+            "There is no target polygon intersecting the slicing boundary.",
+        },
+      })
+    );
   }
 }
